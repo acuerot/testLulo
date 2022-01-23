422bdbc5b2d88ebc0573eb5af75c52f799629f5a
acuerot <angelicacuero@gmail.com>
2022-01-23 15:01:55 -0500

    Proyecto base

---
+++ src/test/java/starter/stepdefinitions/EmpleadoApiStepDefinitions.java
@@ -0,0 +1,24 @@
+package starter.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
+public class EmpleadoApiStepDefinitions {
+
+    @Given("Angelica prepara la consulta")
+    public void angelica_prepara_la_consulta() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @When("ella consulta empleados")
+    public void ella_consulta_empleados() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @Then("ella observa que la consulta de empleados es exitosa")
+    public void ella_observa_que_la_consulta_de_empleados_es_exitosa() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+}
---
+++ src/test/resources/features/empleadoAPI.feature
@@ -0,0 +1,6 @@
+Feature: Operaciones empleados
+
+  Scenario: Consulta empleados
+    Given Angelica prepara la consulta
+    When ella consulta empleados
+    Then ella observa que la consulta de empleados es exitosa
