747687f2b0d6782d039e5837bd621015fe783174
acuerot <angelicacuero@gmail.com>
2022-01-23 15:44:22 -0500

    Proyecto base

--- src/test/java/starter/stepdefinitions/EmpleadoApiStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/EmpleadoApiStepDefinitions.java
@@ -19,6 +19,45 @@ public class EmpleadoApiStepDefinitions {
     @Then("ella observa que la consulta de empleados es exitosa")
     public void ella_observa_que_la_consulta_de_empleados_es_exitosa() {
         // Write code here that turns the phrase above into concrete actions
+    }
+
+    @When("ella consulta un empleado")
+    public void ella_consulta_un_empleado() {
+
+    }
+
+    @Then("ella observa que la consulta del empleado es exitosa")
+    public void ella_observa_que_la_consulta_del_empleado_es_exitosa() {
+
+    }
+
+    @Given("Angelica prepara la creación")
+    public void angelica_prepara_la_creación() {
+
+    }
+
+    @When("ella crea un empleado")
+    public void ella_crea_un_empleado() {
+
+    }
+
+    @Then("ella observa que la creación del empleado es exitosa")
+    public void ella_observa_que_la_creación_del_empleado_es_exitosa() {
+
+    }
+
+    @Given("Angelica prepara la eliminación")
+    public void angelica_prepara_la_eliminación() {
+
+    }
+
+    @When("ella borra un empleado")
+    public void ella_borra_un_empleado() {
+
+    }
+
+    @Then("ella observa que el borrado del empleado es exitosa")
+    public void ella_observa_que_el_borrado_del_empleado_es_exitosa() {
 
     }
 }
--- src/test/resources/features/empleadoAPI.feature
+++ src/test/resources/features/empleadoAPI.feature
@@ -4,3 +4,18 @@ Feature: Operaciones empleados
     Given Angelica prepara la consulta
     When ella consulta empleados
     Then ella observa que la consulta de empleados es exitosa
+
+  Scenario: Consulta empleado
+    Given Angelica prepara la consulta
+    When ella consulta un empleado
+    Then ella observa que la consulta del empleado es exitosa
+
+  Scenario: Crear empleado
+    Given Angelica prepara la creación
+    When ella crea un empleado
+    Then ella observa que la creación del empleado es exitosa
+
+  Scenario: Borrar empleado
+    Given Angelica prepara la eliminación
+    When ella borra un empleado
+    Then ella observa que el borrado del empleado es exitosa
